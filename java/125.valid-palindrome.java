ec77b2c3ee2da822785c18137bd47b7844feec1f
XIX <xiuming.x@outlook.com>
2020-01-27 12:54:02 -0800

    cleanup

--- java/125.valid-palindrome.java
+++ java/125.valid-palindrome.java
@@ -10,8 +10,15 @@ class Solution {
         // char[] arr = s.replaceAll("\\s|,|:", "").toLowerCase().toCharArray();
         char[] arr = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase().toCharArray();
         int start = 0, end = arr.length - 1;
-        while (start < end && arr[start++] == arr[end--])
-            ;
+        // while (start < end && arr[start++] == arr[end--]) 不行
+        while (start < end && arr[start] == arr[end]) {
+            start++;
+            end--;
+            // System.out.println(start + " " + end);
+        }
+
+        // System.out.println(Arrays.toString(arr));
+        // System.out.println(start + " " + end);
         if (start < end)
             return false;
         return true;
---
+++ java/345.reverse-vowels-of-a-string.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=345 lang=java
+ *
+ * [345] Reverse Vowels of a String
+ */
+
+// @lc code=start
+class Solution {
+    public String reverseVowels(String s) {
+        // int vNum = 0;
+        // for (int i = 0; i < s.length(); i++) {
+
+        // }
+        // "aiueo".contains(a);
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println(("aiueo".contains("a")));
+    }
+}
+// @lc code=end
