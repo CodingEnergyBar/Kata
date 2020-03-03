c53dc1ab24cc915b4b5336a6ac3434fe4ce75e98
xiumingxu <gracexuxiuming@gmail.com>
2020-03-03 08:51:32 -0800

    string + dfs

--- java/17.letter-combinations-of-a-phone-number.java
+++ java/17.letter-combinations-of-a-phone-number.java
@@ -7,15 +7,44 @@
 // @lc code=start
 class Solution {
 
-    private List<String> res = new ArrayList<>();
 
+    List<String> res;
+    String[] hash = new String[]{"", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
     public List<String> letterCombinations(String digits) {
-        String[] letterMap = [
-            " ",
-            "",
-            
-        ]
+       
+        res = new ArrayList<String>();
+
+        //path stringbuilder???
+        if(digits.length() == 0)
+            return res;
+
+        combo(digits, 0, new StringBuilder());
+
+        return res;
+   
 
     }
+
+    // public StringBuilder deleteCharAt(int index)
+
+    void combo(String digits, int index,  StringBuilder path){
+        if(index == digits.length()){
+            res.add(path.toString());
+            return;
+        }
+        // "1".matches(regex)
+        // if(!digits.charAt(index).matches("[1-9]"))
+        char cur  =  digits.charAt(index);
+        if(cur < '1' || cur > '9')
+            return ;
+
+        String charArr =  hash[cur - '1'];
+        for(char a: charArr.toCharArray()){
+            path.append(a);
+            combo(digits, index+1, path);
+            path.deleteCharAt(index);
+        }
+    }
+ 
 }
 // @lc code=end
