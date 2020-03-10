65658ea5dc0e8da653018c471e8980442a140480
xiumingxu <gracexuxiuming@gmail.com>
2020-03-10 12:42:15 -0700

    use priorityqueue and comparitor

--- java/973.k-closest-points-to-origin.java
+++ java/973.k-closest-points-to-origin.java
@@ -1,3 +1,4 @@
+import java.util.PriorityQueue;
 import java.util.TreeMap;
 
 /*
@@ -10,7 +11,24 @@ import java.util.TreeMap;
 class Solution {
     public int[][] kClosest(int[][] points, int K) {
         //比较器的写法在复习一下
-        new TreeMap()
+        if(points == null || points.length < K){
+            return points;
+        }
+        PriorityQueue<int[]> q = new PriorityQueue<>((int[] p1, int[] p2)->{
+            int x1 = p1[0], y1 = p1[1];
+            int x2 = p2[0], y2 = p2[1];
+            int d1 =  x1*x1 + y1 * y1;
+            int d2 = x2 * x2 + y2* y2;
+            return d1 - d2;
+        } );
+        for(int[] p:points){
+            q.offer(p);
+        }
+        int[][] res = new int[K][];
+        for(int i=0; i<K; i++){
+            res[i] = q.poll();
+        }
+        return res;
         
     }
 }
