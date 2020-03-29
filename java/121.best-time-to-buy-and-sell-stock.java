4ac6932b54a0df0502b6dff835d57d11083ec557
XIX <xiuming.x@outlook.com>
2020-03-29 11:23:22 -0700

    test setup

--- java/121.best-time-to-buy-and-sell-stock.java
+++ java/121.best-time-to-buy-and-sell-stock.java
@@ -7,16 +7,16 @@
 // @lc code=start
 class Solution {
     public int maxProfit(int[] prices) {
-        //跟时间是没有关系的, 只跟数值有关系
+        // 跟时间是没有关系的, 只跟数值有关系
         int buyDay = 0;
         int maxProfit = 0;
 
-        for(int i=0; i<prices.length;i++){
-            int profit = prices[i]  - prices[buyDay];
-            if(profit > maxProfit){
+        for (int i = 0; i < prices.length; i++) {
+            int profit = prices[i] - prices[buyDay];
+            if (profit > maxProfit) {
                 maxProfit = profit;
             }
-            if(prices[i] < prices[buyDay]){
+            if (prices[i] < prices[buyDay]) {
                 buyDay = i;
             }
         }
