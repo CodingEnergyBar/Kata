99bd8e3c005779e5dace5f5450c119005359e3c2
XIX <xiuming.x@outlook.com>
2020-12-16 08:44:45 -0800

    selectSort

--- js/sort/bubbleSort.js
+++ js/sort/bubbleSort.js
@@ -1,14 +1,21 @@
 Array.prototype.bubbleSort = function () {
+    let times = 0;
     for (let i = 0; i < this.length - 1; i += 1) {
+        let swapped = false;
         for (let j = 0; j < this.length - 1 - i; j += 1) {
             if (this[j] > this[j + 1]) {
+                swapped = true;
+                times++;
                 const temp = this[j];
                 this[j] = this[j + 1];
                 this[j + 1] = temp;
             }
         }
+        if (!swapped)
+            return;
     }
 };
 
-const arr = [5, 4, 3, 2, 1];
+const arr = [4, 5, 3, 2, 1];
 arr.bubbleSort();
+console.log(arr);
--- js/sort/selectionSort.js
+++ js/sort/selectionSort.js
@@ -1,18 +1,37 @@
 Array.prototype.selectionSort = function () {
-    for (let i = 0; i < this.length - 1; i += 1) {
-        let indexMin = i;
-        for (let j = i; j < this.length; j += 1) {
-            if (this[j] < this[indexMin]) {
-                indexMin = j;
+    // 还是需要 let
+    //到最后一个的时候就不用了 
+    for (let i = 0; i < this.length - 1; i++) {
+        let minIndex = i;
+        // for (let j = i; j < this.length - 1; j++) {
+        for (let j = i; j < this.length; j++) {
+            if (this[j] < this[minIndex]) {
+                minIndex = j;
             }
         }
-        if (indexMin !== i) {
-            const temp = this[i];
-            this[i] = this[indexMin];
-            this[indexMin] = temp;
+        if (minIndex !== i) {
+            const tmp = this[minIndex]
+            this[minIndex] = this[i];
+            this[i] = tmp;
         }
     }
+    // for (let i = 0; i < this.length - 1; i += 1) {
+    //     // i 之前的都是拍好的, 现在开始排 i
+    //     let indexMin = i;
+    //     // 打擂台 找最小值 --> 交换
+    //     for (let j = i; j < this.length; j += 1) {
+    //         if (this[j] < this[indexMin]) {
+    //             indexMin = j;
+    //         }
+    //     }
+    //     if (indexMin !== i) {
+    //         const temp = this[i];
+    //         this[i] = this[indexMin];
+    //         this[indexMin] = temp;
+    //     }
+    // }
 };
 
 const arr = [5, 4, 3, 2, 1];
 arr.selectionSort();
+console.log(arr)
